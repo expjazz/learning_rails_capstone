0cb81056430c82eb830e337b93c7c72b9ece66bf
Expedito Andrade <expeditojazz@gmail.com>
2020-05-07 17:31:31 -0300

    added static pages draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/search'
