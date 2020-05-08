89f6d24f545cf56b186bb0fe6b8165de81e0d40a
Expedito Andrade <expeditojazz@gmail.com>
2020-05-08 07:02:36 -0300

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.4.5', require: false
 gem 'bootstrap', '~> 4.4.1'
+gem 'bcrypt', '3.1.13'
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: valid_email_regex },
+                    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
