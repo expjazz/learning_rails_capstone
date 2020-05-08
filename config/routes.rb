9ac336a4beaa031575453ccfc5c4d9d395264f91
Expedito Andrade <expeditojazz@gmail.com>
2020-05-08 15:05:18 -0300

    added sign-up and users page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/search', to: 'static_pages#search'
   get '/contact', to: 'static_pages#contact'
-  get 'signup', to: 'users#new'
+  get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
