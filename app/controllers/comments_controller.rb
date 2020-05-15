c44e214f6ff7e45aabf03140a8c276ab52e90e80
Expedito Andrade <expeditojazz@gmail.com>
2020-05-15 15:27:25 -0300

    Added comment section for apartments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,12 @@
 
 class CommentsController < ApplicationController
   def new; end
+
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment.user = current_user
+    @comment.apartment_id = params[:apartment_id]
+    @comment.save
+    redirect_to apartment_path(params[:apartment_id])
+  end
 end
